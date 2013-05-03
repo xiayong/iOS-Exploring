b9435ee6bff263aff4c52f6fbd60134ca1d5f174
Xia Yong <xiayong@machost.local>
2013-05-04 02:07:56 +0800

    Create the project View Switcher

---
+++ View Switcher/View Switcher/XYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYAppDelegate.h
+//  View Switcher
+//
+//  Created by Xia Yong on 13-5-4.
+//  Copyright (c) 2013年 Xia Yong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
