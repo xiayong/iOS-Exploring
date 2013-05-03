02be0a6b2fbfd9015488dd47b7ba9b7f5e9907f9
Xia Yong <xiayong@machost.local>
2013-05-03 18:53:36 +0800

    Create the project Swap

---
+++ Swap/Swap/XYViewController.m
@@ -0,0 +1,29 @@
+//
+//  XYViewController.m
+//  Swap
+//
+//  Created by Xia Yong on 13-5-3.
+//  Copyright (c) 2013年 Xia Yong. All rights reserved.
+//
+
+#import "XYViewController.h"
+
+@interface XYViewController ()
+
+@end
+
+@implementation XYViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
