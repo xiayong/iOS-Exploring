4202b217c0b094d086ddf118dbae68348f137329
Xia Yong <xiayong@machost.local>
2013-05-06 04:14:40 +0800

    Create the project Pickers.

---
+++ Pickers/Pickers/XYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYAppDelegate.h
+//  Pickers
+//
+//  Created by Xia Yong on 13-5-6.
+//  Copyright (c) 2013年 Xia Yong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pickers/Pickers/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Pickers
+//
+//  Created by Xia Yong on 13-5-6.
+//  Copyright (c) 2013年 Xia Yong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYAppDelegate class]));
+    }
+}
