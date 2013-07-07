13e7e47245a506bbd012f1830851cfae23d9d96b
Xia Yong <xiayong@machost.local>
2013-07-07 22:22:14 +0800

    创建Simple Table工程

---
+++ Simple Table/Simple Table/XYAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  XYAppDelegate.h
+//  Simple Table
+//
+//  Created by Xia Yong on 13-7-7.
+//  Copyright (c) 2013年 Xia Yong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class XYViewController;
+
+@interface XYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) XYViewController *viewController;
+
+@end
---
+++ Simple Table/Simple Table/XYViewController.h
@@ -0,0 +1,13 @@
+//
+//  XYViewController.h
+//  Simple Table
+//
+//  Created by Xia Yong on 13-7-7.
+//  Copyright (c) 2013年 Xia Yong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYViewController : UIViewController
+
+@end
