9deb0b8154e5333ccf57e90919746a137b13e6b9
Xia Yong <xiayong@machost.local>
2013-07-29 19:13:19 +0800

    Create the project 'Simple Storyboard'

---
+++ Simple Storyboards/Simple Storyboards/XYViewController.m
@@ -0,0 +1,29 @@
+//
+//  XYViewController.m
+//  Simple Storyboards
+//
+//  Created by Xia Yong on 13-7-29.
+//  Copyright (c) 2013年 Xia Yong. All rights reserved.
+//
+
+#import "XYViewController.h"
+
+@interface XYViewController ()
+
+@end
+
+@implementation XYViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Simple Storyboards/Simple Storyboards/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Simple Storyboards
+//
+//  Created by Xia Yong on 13-7-29.
+//  Copyright (c) 2013年 Xia Yong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYAppDelegate class]));
+    }
+}
