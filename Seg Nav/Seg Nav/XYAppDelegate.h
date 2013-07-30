250e9206770db4e6abb33c046841e0201784905f
Xia Yong <xiayong@machost.local>
2013-07-31 00:49:53 +0800

    Create the project 'Seg Nav'.

---
+++ Seg Nav/Seg Nav/XYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYAppDelegate.h
+//  Seg Nav
+//
+//  Created by Xia Yong on 13-7-31.
+//  Copyright (c) 2013年 Xia Yong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
