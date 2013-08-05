f1d5280ca5fc83e2ab8a5123a34f0f158a8dd50a
Xia Yong <xiayong@machost.local>
2013-08-06 00:53:40 +0800

    Create the project 'Presidents'.

---
+++ Presidents/Presidents/XYDetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  XYDetailViewController.h
+//  Presidents
+//
+//  Created by Xia Yong on 13-8-5.
+//  Copyright (c) 2013年 Xia Yong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYDetailViewController : UIViewController <UISplitViewControllerDelegate>
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
---
+++ Presidents/Presidents/XYMasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  XYMasterViewController.h
+//  Presidents
+//
+//  Created by Xia Yong on 13-8-5.
+//  Copyright (c) 2013年 Xia Yong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class XYDetailViewController;
+
+@interface XYMasterViewController : UITableViewController
+
+@property (strong, nonatomic) XYDetailViewController *detailViewController;
+
+@end
