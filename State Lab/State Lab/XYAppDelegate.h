99bc296322bcb660359a31b06511a1ba9fe1f224
Xia Yong <xiayong@machost.local>
2013-10-12 01:15:04 +0800

    Create the project 'State Lab'.

---
+++ State Lab/State Lab/XYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYAppDelegate.h
+//  State Lab
+//
+//  Created by Xia Yong on 13-10-11.
+//  Copyright (c) 2013年 Xia Yong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ State Lab/State Lab/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  State Lab
+//
+//  Created by Xia Yong on 13-10-11.
+//  Copyright (c) 2013年 Xia Yong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYAppDelegate class]));
+    }
+}
