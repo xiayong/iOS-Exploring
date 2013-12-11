cfb243af2ffeb8360d00966f769d83f951de283b
xiayong <i.xiayong@me.com>
2013-12-11 22:11:54 +0800

    修改注释

--- TapTaps/TapTaps/XYViewController.m
+++ TapTaps/TapTaps/XYViewController.m
@@ -29,9 +29,9 @@
     UITapGestureRecognizer *doubleTapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tap2)];
     doubleTapRecognizer.numberOfTapsRequired = 2;
     doubleTapRecognizer.numberOfTouchesRequired = 1;
-    // 告诉singleTapRecognizer，它应该在doubleTapRecognizer断定当前用户输入不是doubleTapRecognizer想要的手势时出发自己操作。
+    // 告诉singleTapRecognizer，它应该在doubleTapRecognizer断定当前用户输入不是doubleTapRecognizer想要的手势时触发自己操作。
     // 视图中的单次轻击将立即让singleTapRecognizer认为这是它寻找的手势。于此同时doubleTapRecognizer将认为这看起来适合它，但它需要等待另一次的轻击，因为singleTapRecognizer设置为等待doubleTapRecognizer失败，所以singleTapRecognizer不会立即调用自己的操作方法，而是等待doubleTapRecognizer的结果。
-    // 第一次轻击之后如果，立即发生了第二次轻击，doubleTapRecognizer会完全认为这是我需要的手势，并触发自己的操作。在这是，singleTapRecognizer将意识到所发生的事情，并放弃该手势。另一方面，如果经过了特定时间(系统规定的两次轻击之间最大的时间长度)，doubleTapRecognizer将放弃，singleTapRecognizer看到doubleTapRecognizer失败，最终将触发自己的操作。
+    // 第一次轻击之后，如果立即发生了第二次轻击，doubleTapRecognizer会完全认为这是我需要的手势，并触发自己的操作。在这是，singleTapRecognizer将意识到所发生的事情，并放弃该手势。另一方面，如果经过了特定时间(系统规定的两次轻击之间最大的时间长度)，doubleTapRecognizer将放弃，singleTapRecognizer看到doubleTapRecognizer失败，最终将触发自己的操作。
     [singleTapRecognizer requireGestureRecognizerToFail:doubleTapRecognizer];
     [self.view addGestureRecognizer:doubleTapRecognizer];
     
