fc501e2e549fdf263d8d0ce23621c3a5e2c8ccbe
xiayong <i.xiayong@me.com>
2013-12-12 22:52:20 +0800

    Create The Project WhereAmI

---
+++ WhereAmI/WhereAmI/XYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYAppDelegate.h
+//  WhereAmI
+//
+//  Created by Xia Yong on 13-12-12.
+//  Copyright (c) 2013年 Xia Yong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WhereAmI/WhereAmI/XYViewController.h
@@ -0,0 +1,13 @@
+//
+//  XYViewController.h
+//  WhereAmI
+//
+//  Created by Xia Yong on 13-12-12.
+//  Copyright (c) 2013年 Xia Yong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYViewController : UIViewController
+
+@end
